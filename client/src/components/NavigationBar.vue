77c27bd4201302196ff71190d13271789812539c
istvzsig <istvanzsigmond@aol.com>
2025-01-07 10:22:39 +0100

    update nav bar and jsonconfig

--- client/src/components/NavigationBar.vue
+++ client/src/components/NavigationBar.vue
@@ -6,8 +6,34 @@ const routes = router.getRoutes()
 
 <template>
     <nav>
-        <RouterLink v-for="(route, index) in routes" :key="index" :to="route.path">
-            {{ route.name[0].toUpperCase() + [...route.name].splice(1).join('') }}
-        </RouterLink>
+        <div class="links">
+            <RouterLink v-for="(route, index) in routes" :key="index" :to="route.path">
+                {{ route.name[0].toUpperCase() + [...route.name].splice(1).join('') }}
+            </RouterLink>
+        </div>
+        <button class="booking-button">BOOK</button>
     </nav>
 </template>
+
+<style scoped>
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+nav * {
+    font-weight: bold;
+}
+
+.booking-button {
+    padding: 1rem;
+    border-radius: 10px;
+    color: var(--main-white);
+    font-weight: bold;
+    font-size: 13px;
+    background-color: transparent;
+    background-color: rgba(0, 0, 0, 0) !important;
+    cursor: crosshair;
+}
+</style>
